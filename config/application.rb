38e5ff505e7e3219c9cd25eae2c3f03cfa715dd7
Daniel <danielnottingham.ti@gmail.com>
2020-10-13 13:04:29 -0300

    starting the app

---
+++ config/application.rb
@@ -0,0 +1,30 @@
+require_relative 'boot'
+
+#require 'rails/all'
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/railtie'
+require 'active_controller/railtie'
+require 'active_mailer/railtie'
+require 'active_view/railtie'
+require 'active_cable/engine'
+require 'sprockets/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TimeToAnswer
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    config.generators.system_tests = nil
+  end
+end
